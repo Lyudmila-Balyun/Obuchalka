852bd0556f0b648dd0b788dda8ed9e09c5fb1538
Людмила <severianka1@yandex.ru>
2024-09-08 22:16:07 +0300

    lesson2

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-example = "Глазовыколупывательница"
-print(example[0])
-print(example[-1])
-print(example[11:])
-print(example[::-1])
-print(example[0::2])
+lessons = 12
+hours = 1.5
+name_of_course = 'Python'
+time_for_lesson = hours / lessons
+print('Курс: ' + name_of_course + ', ' + 'всего задач: ' + str(lessons) + ', ' + 'затрачено часов: ' + str(
+    hours) + ', ' + 'среднее время выполнения: ' + str(time_for_lesson))
+print(
+    f'Курс: {name_of_course}, всего задач: {lessons}, затрачено часов: {hours}, среднее время выполнения: {time_for_lesson}')
